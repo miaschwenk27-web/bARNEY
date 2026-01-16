46f49287e0df63184cc864825a6c040c5c6fb5e0
miaschwenk27-web <miaschwenk27@gmail.com>
2026-01-16 12:44:40 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -15,6 +15,49 @@ function turn_left () {
     nezhaV2.move(nezhaV2.MotorPostion.M1, 30, nezhaV2.MovementDirection.CW, 176, nezhaV2.SportsMode.Degree, nezhaV2.DelayMode.NoDelay)
     nezhaV2.move(nezhaV2.MotorPostion.M2, 30, nezhaV2.MovementDirection.CW, 176, nezhaV2.SportsMode.Degree, nezhaV2.DelayMode.NoDelay)
 }
+function led_display () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(5)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.pause(5)
+    basic.showIcon(IconNames.SmallSquare)
+    basic.pause(5)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.pause(5)
+    basic.showIcon(IconNames.Square)
+    basic.pause(5)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.pause(5)
+    basic.showIcon(IconNames.SmallSquare)
+    basic.pause(5)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.pause(5)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+}
 function forward () {
     nezhaV2.move(nezhaV2.MotorPostion.M1, 50, nezhaV2.MovementDirection.CCW, 566, nezhaV2.SportsMode.Degree, nezhaV2.DelayMode.NoDelay)
     nezhaV2.move(nezhaV2.MotorPostion.M2, 50, nezhaV2.MovementDirection.CW, 566, nezhaV2.SportsMode.Degree, nezhaV2.DelayMode.NoDelay)
@@ -37,19 +80,8 @@ for (let index = 0; index < 5; index++) {
     forward()
     basic.pause(2000)
     _180()
-    basic.showIcon(IconNames.SmallDiamond)
-    basic.pause(100)
-    basic.clearScreen()
-    basic.showIcon(IconNames.SmallSquare)
-    basic.pause(100)
-    basic.clearScreen()
-    basic.showIcon(IconNames.Square)
-    basic.pause(100)
-    basic.clearScreen()
-    basic.showIcon(IconNames.SmallSquare)
-    basic.pause(100)
-    basic.clearScreen()
-    basic.showIcon(IconNames.SmallDiamond)
+    basic.pause(50)
+    led_display()
 }
 basic.forever(function () {
 	
